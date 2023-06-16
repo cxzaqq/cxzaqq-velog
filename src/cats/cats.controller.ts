39c83ceda7abff2ddbd2239c101ee69adfc7a5ec
cxzaqq <roy1228@naver.com>
2023-06-16 21:15:38 +0900

    controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { CreateCatDto } from '../cats/dto/create-cat.dto';
+import { UpdateCatDto } from '../cats/dto/update-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  findAll() {
+    return `This action returns all cats`;
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
   }
 }
