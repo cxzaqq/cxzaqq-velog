935973c32b3ef8328783bab4bd2162f2f251c2c4
cxzaqq <roy1228@naver.com>
2023-08-19 16:53:06 +0900

    3.3-dynamic-modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,68 +1,11 @@
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import {
-  LoggerMiddleware,
-  logger,
-} from './common/middleware/logger.middleware';
-import { CatsController } from './cats/cats.controller';
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { HttpExceptionFilter } from './common/filter/http-exception.filter';
-import { RolesGuard } from './roles/roles.guard';
-import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
-import { TransformInterceptor } from './common/interceptor/transform.interceptor';
-import { ExcludeNullInterceptor } from './common/interceptor/excludeNull.interceptor';
-import { ErrorsInterceptor } from './common/interceptor/errors.interceptor';
-import { TimeoutInterceptor } from './common/interceptor/timeout.interceptor';
-import { UserModule } from './user/user.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [CatsModule, UserModule],
+  imports: [ConfigModule.register({ folder: './config' })],
   controllers: [AppController],
-  providers: [
-    AppService,
-    // {
-    //   provide: APP_FILTER,
-    //   useClass: HttpExceptionFilter,
-    // },
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: RolesGuard,
-    // },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: TimeoutInterceptor,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
-/* class-based
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(logger).forRoutes(CatsController);
-  }
-}
-*/
-/*
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .exclude(
-        {
-          path: 'cats',
-          method: RequestMethod.GET,
-        },
-        { path: 'cats', method: RequestMethod.POST },
-        `cats/(.*)`,
-      )
-      .forRoutes(CatsController);
-  }
-}
-*/
