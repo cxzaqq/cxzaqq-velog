a2bb1bc4713402ff0dfdbb0c92364366594af4ea
cxzaqq <roy1228@naver.com>
2023-09-20 01:30:07 +0900

    3.5-circular-dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.register({ folder: './config' })],
+  imports: [AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
