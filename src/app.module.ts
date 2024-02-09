0fe312adb138dbc6a1a413e5b003d2fbdd7d3eb9
cxzaqq <roy1228@naver.com>
2024-02-09 19:46:56 +0900

    초기화

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { CatsModule } from './cats/cats.module';
-import { APP_GUARD } from '@nestjs/core';
-import { RolesGuard } from './roles/roles.guard';
 
 @Module({
-  imports: [AuthModule, UserModule, CatsModule],
+  imports: [],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: RolesGuard }],
+  providers: [AppService],
 })
 export class AppModule {}
